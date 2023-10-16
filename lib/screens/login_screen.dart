b144a988b51bf6a18d9594eee2eb4af6191e6faa
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-10-16 14:23:29 +0700

    feat(login): Update login Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -27,23 +27,60 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(
+            'assets/images/logoHutech.png',
+          ),
+        ),
         title: const Text("Đăng nhập"),
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(
-          child: Column(
-            children: [
-              LoginForm(
-                onLogin: (userName, password) async {
-                  developer.log('Username: $userName, Password: $password');
-                  var isSucceed = await userStore.login(userName, password);
-                  if (isSucceed && context.mounted) {
-                    Navigator.pushReplacementNamed(context, RouteManager.home);
-                  }
-                },
+          child: Card(
+            elevation: 5, // Độ nâng cao của card
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10), // Độ cong viền của card
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Center(
+                      child: Image.asset(
+                        'assets/images/logo-hutech-classroom.png',
+                        height: 200,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Center(
+                        child: LoginForm(
+                          onLogin: (userName, password) async {
+                            developer.log(
+                                'Username: $userName, Password: $password');
+                            var isSucceed =
+                                await userStore.login(userName, password);
+                            if (isSucceed && context.mounted) {
+                              Navigator.pushReplacementNamed(
+                                  context, RouteManager.home);
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),

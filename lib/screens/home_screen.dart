df2cd19540070d54aa604dfd6598ced3b517e3e3
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-10-16 22:52:56 +0700

    feat(home): Update home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key, required this.title});
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -14,7 +14,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
   late UserStore _userStore;
 
   @override
@@ -23,79 +22,167 @@ class _HomeScreenState extends State<HomeScreen> {
     _userStore = context.read<UserStore>();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter += 100;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(
-            widget.title,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              child: const Text('Cài đặt'),
+            ),
+            ListTile(
+              title: Observer(builder: (context) {
+                return Row(
+                  children: [
+                    const Text("Sinh viên: "),
+                    Text(_userStore.user.userName ?? 'Bạn chưa đăng nhập'),
+                  ],
+                );
+              }),
+            ),
+            ListTile(
+              title: FloatingActionButton(
+                heroTag: "LogButton",
+                child: Builder(builder: (context) {
+                  return Text(
+                      _userStore.user.id != null ? 'Đăng xuất' : 'Đăng nhập');
+                }),
+                onPressed: () {
+                  if (_userStore.user.id != null) {
+                    _userStore.resetValue();
+                  }
+                  Navigator.pushReplacementNamed(context, RouteManager.login);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 70),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 4),
+                  child: Text(
+                    'Chào mừng bạn đã trở lại!',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Observer(builder: (context) {
+                    return Column(
+                      children: [
+                        Text(
+                          '${_userStore.user.userName ?? 'N/A'} - ${_userStore.user.lastName ?? 'N/A'} ${_userStore.user.firstName ?? 'N/A'}',
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    );
+                  }),
+                ),
+                Image.asset(
+                  'assets/images/pic1_Data-analysis-amico.png',
+                  height: 350,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // Xử lý khi nút "Xem Bảng Điểm" được nhấn
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.all(20),
+                                  minimumSize: const Size(200, 200),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                ),
+                                child: const Column(
+                                  children: [
+                                    Icon(
+                                      Icons.table_chart,
+                                      size: 50,
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      'Xem Bảng Điểm',
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // Xử lý khi nút "Scan Kiểm Tra" được nhấn
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.all(20),
+                                  minimumSize: const Size(200, 200),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                ),
+                                child: const Column(
+                                  children: [
+                                    Icon(
+                                      Icons.check_box,
+                                      size: 50,
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      'Scan Kiểm Tra',
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.inversePrimary,
-                  ),
-                  child: const Text('Cài đặt')),
-              ListTile(
-                title: Observer(builder: (context) {
-                  return Row(
-                    children: [
-                      const Text("Sinh viên: "),
-                      Text(_userStore.user.userName ?? 'Bạn chưa đăng nhập'),
-                    ],
-                  );
-                }),
-              ),
-              ListTile(
-                title: FloatingActionButton(
-                  heroTag: "LogButton",
-                  child: Builder(builder: (context) {
-                    return Text(
-                        _userStore.user.id != null ? 'Đăng xuất' : 'Đăng nhập');
-                  }),
-                  onPressed: () {
-                    if (_userStore.user.id != null) {
-                      _userStore.resetValue();
-                    }
-                    Navigator.pushReplacementNamed(context, RouteManager.login);
-                  },
-                ),
-              )
-            ],
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Số lần nhấn:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          heroTag: "Increment",
-          onPressed: _incrementCounter,
-          tooltip: 'Tăng',
-          child: const Icon(Icons.add),
-        ));
+      ),
+    );
   }
 }

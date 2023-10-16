a1045bc12e18edf748b8966822600a4b6cd49480
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-10-16 16:15:06 +0700

    refactor(login): Update UI Login Screen and update Form

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,7 +7,7 @@ import 'dart:developer' as developer;
 import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -33,52 +33,78 @@ class _LoginScreenState extends State<LoginScreen> {
             'assets/images/logoHutech.png',
           ),
         ),
-        title: const Text("Đăng nhập"),
+        title: const Text(
+          "HUTECH CLASSROOM",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Card(
-            elevation: 5, // Độ nâng cao của card
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10), // Độ cong viền của card
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Center(
-                      child: Image.asset(
-                        'assets/images/logo-hutech-classroom.png',
-                        height: 200,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Center(
-                        child: LoginForm(
-                          onLogin: (userName, password) async {
-                            developer.log(
-                                'Username: $userName, Password: $password');
-                            var isSucceed =
-                                await userStore.login(userName, password);
-                            if (isSucceed && context.mounted) {
-                              Navigator.pushReplacementNamed(
-                                  context, RouteManager.home);
-                            }
-                          },
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: IntrinsicHeight(
+                child: Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/images/logo-hutech-classroom.png',
+                              height: 200,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 5),
+                              child: Text(
+                                'HỆ THỐNG SCAN KIỂM TRA BẢNG ĐIỂM SINH VIÊN',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(50.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              LoginForm(
+                                onLogin: (userName, password) async {
+                                  developer.log(
+                                      'Username: $userName, Password: $password');
+                                  var isSucceed =
+                                      await userStore.login(userName, password);
+                                  if (isSucceed && context.mounted) {
+                                    Navigator.pushReplacementNamed(
+                                        context, RouteManager.home);
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),

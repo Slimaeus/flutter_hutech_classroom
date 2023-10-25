f23e0c7c4b80209e91bc699d57b6111df6db45ee
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-10-25 16:30:02 +0700

    refactor(drawer): Update custom drawer

--- lib/widgets/layout/custom_drawer.dart
+++ lib/widgets/layout/custom_drawer.dart
@@ -1,46 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hutech_classroom/managers/route_manager.dart';
 import 'package:flutter_hutech_classroom/stores/user_store.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
 Widget customDrawer(BuildContext context) {
   UserStore userStore = context.read<UserStore>();
 
   return Drawer(
-    
     child: ListView(
-      children: [
-        DrawerHeader(
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.inversePrimary,
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          child: UserAccountsDrawerHeader(
+            accountName: Text(
+              '${userStore.user.lastName ?? 'N/A'} ${userStore.user.firstName ?? 'N/A'}',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            accountEmail: Text(
+              userStore.user.userName ?? 'Bạn chưa đăng nhập',
+              style: const TextStyle(fontSize: 16),
+            ),
+            currentAccountPicture: const Padding(
+              padding: EdgeInsets.only(bottom: 8.0),
+              child: CircleAvatar(
+                child: Icon(
+                  Icons.person,
+                  size: 55,
+                ),
+              ),
+            ),
           ),
-          child: const Text('Cài đặt'),
         ),
         ListTile(
-          title: Observer(builder: (context) {
-            return Row(
-              children: [
-                const Text("Sinh viên: "),
-                Text(userStore.user.userName ?? 'Bạn chưa đăng nhập'),
-              ],
-            );
-          }),
+          leading: const Icon(Icons.home),
+          title: const Text('Trang Chủ'),
+          onTap: () {
+            Navigator.pushReplacementNamed(context, RouteManager.home);
+          },
         ),
         ListTile(
-          title: FloatingActionButton(
-            heroTag: "LogButton",
-            child: Builder(builder: (context) {
-              return Text(
-                  userStore.user.id != null ? 'Đăng xuất' : 'Đăng nhập');
-            }),
-            onPressed: () {
-              if (userStore.user.id != null) {
-                userStore.resetValue();
-              }
-              Navigator.pushReplacementNamed(context, RouteManager.login);
-            },
-          ),
+          leading: const Icon(Icons.assignment),
+          title: const Text('Xem Bảng Điểm'),
+          onTap: () {
+            Navigator.pushReplacementNamed(
+                context, RouteManager.studentTranscript);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.scanner),
+          title: const Text('Scan Kiểm Tra'),
+          onTap: () {
+            Navigator.pushReplacementNamed(context, RouteManager.scan);
+          },
+        ),
+        const Divider(),
+        ListTile(
+          leading: const Icon(Icons.person),
+          title: const Text('Hồ Sơ Cá Nhân'),
+          onTap: () {
+            // Điều hướng đến trang Hồ Sơ Cá Nhân
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.exit_to_app),
+          title: const Text('Đăng Xuất'),
+          onTap: () {
+            if (userStore.user.id != null) {
+              userStore.resetValue();
+            }
+            Navigator.pushReplacementNamed(context, RouteManager.login);
+          },
         ),
       ],
     ),

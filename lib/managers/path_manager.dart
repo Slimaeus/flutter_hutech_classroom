b4e2a04217ac564d0d9eb1223b51ff8ce160b484
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-10-26 20:02:57 +0700

    feat(path): Add new logo imge

--- lib/managers/path_manager.dart
+++ lib/managers/path_manager.dart
@@ -2,6 +2,7 @@ class PathManager {
   static String banner1 = 'assets/images/banner1.png';
   static String banner2 = 'assets/images/banner2.png';
   static String banner3 = 'assets/images/banner3.png';
+  static String logoHutechScanSystem = 'assets/images/logo-hutech-scansystem.png';
   static String logoHutechClassroom = 'assets/images/logo-hutech-classroom.png';
   static String logoHutechAdministration = 'assets/images/logo-hutech-administration.png';
   static String logoHutech = 'assets/images/logoHutech.png';

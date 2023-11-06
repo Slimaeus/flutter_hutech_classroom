134caad7c2fb7c53e4cdf7404a1c7e1b756beec8
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-11-06 21:07:54 +0700

    feat(details): Update Card Info for details screen

--- lib/screens/student_transcript_details_screen.dart
+++ lib/screens/student_transcript_details_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter_hutech_classroom/widgets/layout/custom_appbar.dart';
 import 'package:flutter_hutech_classroom/widgets/layout/custom_drawer.dart';
 
 class StudentTranscriptDetailsScreen extends StatelessWidget {
-  const StudentTranscriptDetailsScreen({super.key, required this.title});
+  const StudentTranscriptDetailsScreen({Key? key, required this.title})
+      : super(key: key);
 
   final String title;
 
@@ -12,7 +13,54 @@ class StudentTranscriptDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: customAppBar(context, title: title, hasLeading: true),
       endDrawer: customDrawer(context),
-      body: const Text('data'),
-      );
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 3,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildInfoRow('Năm học', '2023'),
+                      _buildInfoRow('Học kỳ', '1'),
+                      _buildInfoRow('Mã học phần', 'ABC123'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildInfoRow('Học phần', 'Tên học phần'),
+                      _buildInfoRow('Số tín chỉ', '3'),
+                      _buildInfoRow('Nhóm', 'Group A'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInfoRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Text(
+            '$label: ',
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(value),
+        ],
+      ),
+    );
   }
 }

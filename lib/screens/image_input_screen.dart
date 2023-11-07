73762ea1457dd8459e2367736052d08cf3aba6eb
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-11-07 22:50:42 +0700

    feat(imageinput): Update UI

--- lib/screens/image_input_screen.dart
+++ lib/screens/image_input_screen.dart
@@ -17,16 +17,15 @@ class ImageInputScreen extends StatelessWidget {
         child: Center(
             child: Padding(
           padding: EdgeInsets.symmetric(
-              vertical: MediaQuery.of(context).size.height * 0.1,
+              vertical: MediaQuery.of(context).size.height * 0.01,
               horizontal: MediaQuery.of(context).size.width * 0.25),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   foregroundColor: Colors.white,
                   backgroundColor: Colors.blue,
-                  minimumSize: const Size(double.infinity, 50),
                   padding: const EdgeInsets.all(20),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(5),
@@ -34,11 +33,54 @@ class ImageInputScreen extends StatelessWidget {
                 ),
                 onPressed: () {
                   // Handle the submit button press event
-                  Navigator.pushNamed(
-                      context, RouteManager.scan);
                 },
                 child: const Text(
-                  'IMPORT',
+                  'NHẬP ẢNH',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              //TODO: Handle cắt ảnh tại đây!
+              const SizedBox(
+                width: double.infinity,
+                height: 500,
+                child: Card(
+                  elevation: 3,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ảnh',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                onPressed: () {
+                  // Handle the submit button press event
+                  Navigator.pushNamed(context, RouteManager.scan);
+                },
+                child: const Text(
+                  'TIẾP TỤC',
                   style: TextStyle(
                     fontSize: 18,
                   ),

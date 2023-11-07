0ec034bd898ff5fc80957f281588cd82139eeb8f
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-11-07 20:20:43 +0700

    feat(scan): Update button

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hutech_classroom/managers/route_manager.dart';
 import 'package:flutter_hutech_classroom/widgets/layout/custom_appbar.dart';
 import 'package:flutter_hutech_classroom/widgets/layout/custom_drawer.dart';
 
@@ -12,7 +13,41 @@ class ScanScreen extends StatelessWidget {
     return Scaffold(
       appBar: customAppBar(context, title: title, hasLeading: true),
       endDrawer: customDrawer(context),
-      body: const Text('data'),
+      body: SingleChildScrollView(
+        child: Center(
+            child: Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height * 0.1,
+              horizontal: MediaQuery.of(context).size.width * 0.25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  minimumSize: const Size(double.infinity, 50),
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                onPressed: () {
+                  // Handle the submit button press event
+                  Navigator.pushNamed(
+                      context, RouteManager.home);
+                },
+                child: const Text(
+                  'IMPORT',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }

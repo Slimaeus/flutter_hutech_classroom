303123275af96c3ea25b30803a6b0a5a072c221e
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-11-08 10:32:44 +0700

    feat(scan): Add File Picker

--- lib/screens/image_input_screen.dart
+++ lib/screens/image_input_screen.dart
@@ -1,17 +1,26 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hutech_classroom/managers/route_manager.dart';
 import 'package:flutter_hutech_classroom/widgets/layout/custom_appbar.dart';
 import 'package:flutter_hutech_classroom/widgets/layout/custom_drawer.dart';
 
-class ImageInputScreen extends StatelessWidget {
+class ImageInputScreen extends StatefulWidget {
   const ImageInputScreen({super.key, required this.title});
 
   final String title;
 
   @override
+  State<ImageInputScreen> createState() => _ImageInputScreenState();
+}
+
+class _ImageInputScreenState extends State<ImageInputScreen> {
+  File? _file;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: customAppBar(context, title: title, hasLeading: true),
+      appBar: customAppBar(context, title: widget.title, hasLeading: true),
       endDrawer: customDrawer(context),
       body: SingleChildScrollView(
         child: Center(
@@ -31,8 +40,18 @@ class ImageInputScreen extends StatelessWidget {
                     borderRadius: BorderRadius.circular(5),
                   ),
                 ),
-                onPressed: () {
+                onPressed: () async {
                   // Handle the submit button press event
+                  FilePickerResult? result = await FilePicker.platform
+                      .pickFiles(allowedExtensions: ['jpg', 'png']);
+
+                  if (result != null) {
+                    setState(() {
+                      _file = File(result.files.single.path!);
+                    });
+                  } else {
+                    // User canceled the picker
+                  }
                 },
                 child: const Text(
                   'NHẬP ẢNH',
@@ -43,23 +62,25 @@ class ImageInputScreen extends StatelessWidget {
               ),
               const SizedBox(height: 10),
               //TODO: Handle cắt ảnh tại đây!
-              const SizedBox(
+              SizedBox(
                 width: double.infinity,
-                height: 500,
+                height: _file != null ? Image.file(_file as File).height : 500,
                 child: Card(
                   elevation: 3,
                   child: Padding(
-                    padding: EdgeInsets.all(16.0),
+                    padding: const EdgeInsets.all(16.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
+                        const Text(
                           'Ảnh',
                           style: TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
+                        if (_file != null) Text(_file!.path),
+                        if (_file != null) Image.file(_file as File)
                       ],
                     ),
                   ),

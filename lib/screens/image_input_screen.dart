171e12a15ce47cbf717d50016bb150d4beb01093
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-11-08 14:19:05 +0700

    chore(image-input): Change padding

--- lib/screens/image_input_screen.dart
+++ lib/screens/image_input_screen.dart
@@ -27,7 +27,7 @@ class _ImageInputScreenState extends State<ImageInputScreen> {
             child: Padding(
           padding: EdgeInsets.symmetric(
               vertical: MediaQuery.of(context).size.height * 0.01,
-              horizontal: MediaQuery.of(context).size.width * 0.25),
+              horizontal: MediaQuery.of(context).size.width * 0.1),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [

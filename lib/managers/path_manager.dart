9a5ece3296500b187511f857f9b27879e88bad02
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-11-13 21:27:13 +0700

    chore(assets): Add banners image

--- lib/managers/path_manager.dart
+++ lib/managers/path_manager.dart
@@ -2,6 +2,9 @@ class PathManager {
   static String banner1 = 'assets/images/banner1.png';
   static String banner2 = 'assets/images/banner2.png';
   static String banner3 = 'assets/images/banner3.png';
+  static String banner4 = 'assets/images/banner4.png';
+  static String banner5 = 'assets/images/banner5.png';
+  static String banner6 = 'assets/images/banner6.png';
   static String logoHutechScanSystem = 'assets/images/logo-hutech-scansystem.png';
   static String logoHutechClassroom = 'assets/images/logo-hutech-classroom.png';
   static String logoHutechAdministration = 'assets/images/logo-hutech-administration.png';

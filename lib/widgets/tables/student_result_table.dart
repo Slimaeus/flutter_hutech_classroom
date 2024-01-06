9f96d213786df30a3c5a0fb4e3a8ac259f232b54
Slimaeus <nguyenhongthai28042002@gmail.com>
2024-01-06 09:59:12 +0700

    refactor: Update student result table column

--- lib/widgets/tables/student_result_table.dart
+++ lib/widgets/tables/student_result_table.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hutech_classroom/models/student_result.dart';
 import 'package:flutter_hutech_classroom/widgets/tables/custom_table.dart';
@@ -7,36 +9,41 @@ import 'package:flutter_hutech_classroom/widgets/tables/custom_table_header_cell
 Widget studentResultTable(List<StudentResult> studentResult) {
   return customTable([
     customTableHeaderCell('STT'),
-    customTableHeaderCell('Mã'),
-    customTableHeaderCell('Họ'),
-    customTableHeaderCell('Tên'),
-    customTableHeaderCell('Lớp'),
+    customTableHeaderCell('MSSV'),
+    if (Platform.isWindows) ...[
+      customTableHeaderCell('Họ'),
+      customTableHeaderCell('Tên'),
+      customTableHeaderCell('Lớp'),
+    ],
     customTableHeaderCell('Điểm'),
     if (studentResult.any((element) => element.comparedScore != null))
       customTableHeaderCell('Điểm hệ thống'),
   ], [
     ...studentResult.map((r) => [
           customTableCell(r.ordinalNumber.toString()),
-          if (r.student != null &&
-              r.student?.userName != null &&
-              r.student?.firstName != null &&
-              r.student?.lastName != null) ...[
+          if (r.student != null && r.student?.userName != null)
             customTableCell(r.student!.userName!),
-            customTableCell(r.student!.lastName!, alignLeft: true),
-            customTableCell(r.student!.firstName!, alignLeft: true),
-          ] else ...[
-            customTableCell(r.studentId ?? ""),
-            customTableCell("", alignLeft: true),
-            customTableCell("", alignLeft: true),
-          ],
-          if (r.classroom != null && r.classroom?.className != null)
-            customTableCell(r.classroom!.className!, alignLeft: true)
-          else if (r.student?.className != null)
-            customTableCell(
-                r.student?.className != null ? r.student!.className! : "",
-                alignLeft: true)
-          else
-            customTableCell("", alignLeft: true),
+          if (Platform.isWindows)
+            if (r.student != null &&
+                r.student?.userName != null &&
+                r.student?.firstName != null &&
+                r.student?.lastName != null) ...[
+              customTableCell(r.student!.lastName!, alignLeft: true),
+              customTableCell(r.student!.firstName!, alignLeft: true),
+            ] else ...[
+              customTableCell(r.studentId ?? ""),
+              customTableCell("", alignLeft: true),
+              customTableCell("", alignLeft: true),
+            ],
+          if (Platform.isWindows)
+            if (r.classroom != null && r.classroom?.className != null)
+              customTableCell(r.classroom!.className!, alignLeft: true)
+            else if (r.student?.className != null)
+              customTableCell(
+                  r.student?.className != null ? r.student!.className! : "",
+                  alignLeft: true)
+            else
+              customTableCell("", alignLeft: true),
           customTableCell(
               r.score != null && r.score != -1 ? r.score.toString() : "",
               color: r.comparedScore != null
